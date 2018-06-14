48911eb594c2d3b4f3cc080d4c6fc23f39397aed
alexandre <alexandrveira@gmail.com>
2018-06-14 11:35:00 -0300

    trabalhando_fornecedores

--- src/br/com/farmacia/bean/FornecedoresBean.java
+++ src/br/com/farmacia/bean/FornecedoresBean.java
@@ -10,10 +10,21 @@ import javax.faces.model.ListDataModel;
 
 import br.com.farmacia.DAO.FornecedoresDAO;
 import br.com.farmacia.domain.Fornecedores;
+import br.com.farmacia.util.JSFUtil;
 
 @ManagedBean(name = "MBFornecedores")
 @ViewScoped
 public class FornecedoresBean {
+
+	private Fornecedores fornecedores;
+
+	public Fornecedores getFornecedores() {
+		return fornecedores;
+	}
+
+	public void setFornecedores(Fornecedores fornecedores) {
+		this.fornecedores = fornecedores;
+	}
 
 	private ListDataModel<Fornecedores> itens;
 
@@ -30,6 +41,7 @@ public class FornecedoresBean {
 	@PostConstruct
 	public void prepararPesquisa() {
         
+		
 		try {
 			FornecedoresDAO fd = new FornecedoresDAO();
 			ArrayList<Fornecedores> lista = fd.listar();
@@ -39,6 +51,31 @@ public class FornecedoresBean {
 		}
 
 		catch (SQLException e) {
+            JSFUtil.adicionarMensagemErro("ex.getMessage()"); 
+			e.printStackTrace();
+		}
+
+	}
+
+	public void preparanovo() {
+
+		fornecedores = new Fornecedores();
+
+	}
+
+	public void novo() {
+
+		try {
+			FornecedoresDAO fd = new FornecedoresDAO();
+			fd.salvar(fornecedores);
+
+			ArrayList<Fornecedores> lista = fd.listar();
+
+			itens = new ListDataModel<Fornecedores>(lista);
+			
+			JSFUtil.adicionarMensagemSucesso("Fornecedor salvo com sucesso");
+
+		} catch (SQLException e) {
 
 			e.printStackTrace();
 		}
